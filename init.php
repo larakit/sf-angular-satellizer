1092953ab7c7f2733a0a6ba857ff4ac3ab2d2c09
Berdnikov Alexey <aberdnikov@gmail.com>
2016-07-06 08:38:44 +0500

    Create init.php

---
+++ init.php
@@ -0,0 +1,6 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-angular-satellizer')
+    ->usePackage('larakit/sf-angular')
+    ->ngModule('satellizer')
+    ->js('//cdnjs.cloudflare.com/ajax/libs/satellizer/0.14.1/satellizer.min.js');
+
