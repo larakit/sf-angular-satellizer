6bb61e264a54ce68c029f5a7c0099ead7ca03f9e
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 21:42:13 +0500

    fixed

--- init.php
+++ init.php
@@ -2,5 +2,6 @@
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-satellizer')
     ->usePackage('larakit/sf-angular')
     ->ngModule('satellizer')
-    ->js('//cdnjs.cloudflare.com/ajax/libs/satellizer/0.14.1/satellizer.min.js');
+    ->setSourceDir('public')
+    ->jsPackage('satellizer.min.js');
 
