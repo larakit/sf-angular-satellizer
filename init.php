d12a7826f4f358362cc55aebc4b1116b4914ba7a
Berdnikov Alexey <aberdnikov@gmail.com>
2017-04-20 09:02:57 +0500

    Update init.php

--- init.php
+++ init.php
@@ -1,7 +1,6 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-satellizer')
     ->usePackage('larakit/sf-angular')
-    ->ngModule('satellizer')
     ->setSourceDir('public')
     ->jsPackage('satellizer.min.js');
 
